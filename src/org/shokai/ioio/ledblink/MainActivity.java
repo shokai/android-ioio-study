6f00f5cbb17488f71e11aa8ac7ece91f3c0bd28a
Sho Hashimoto <hashimoto@shokai.org>
2011-07-18 07:53:47 +0900

    toggle LEDs by Buttons

--- src/org/shokai/ioio/ledblink/MainActivity.java
+++ src/org/shokai/ioio/ledblink/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.View.OnClickListener;
 import android.widget.*;
 
 public class MainActivity extends AbstractIOIOActivity implements View.OnClickListener {
-	ToggleButton[] btn_led = new ToggleButton[3];
+	ToggleButton btn_led0, btn_led1, btn_led2;
 	ToggleButton btn_all;
 	
     @Override
@@ -20,12 +20,14 @@ public class MainActivity extends AbstractIOIOActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         log("start");
-        this.btn_led[0] = (ToggleButton)this.findViewById(R.id.toggleButtonLED0);
-        this.btn_led[1] = (ToggleButton)this.findViewById(R.id.toggleButtonLED1);
-        this.btn_led[2] = (ToggleButton)this.findViewById(R.id.toggleButtonLED2);
-        for(int i = 0; i < btn_led.length; i++){
-        	btn_led[i].setOnClickListener(this);
-        }
+        
+        this.btn_led0 = (ToggleButton)this.findViewById(R.id.toggleButtonLED0);
+        this.btn_led1 = (ToggleButton)this.findViewById(R.id.toggleButtonLED1);
+        this.btn_led2 = (ToggleButton)this.findViewById(R.id.toggleButtonLED2);
+	    btn_led0.setOnClickListener(this);
+	    btn_led1.setOnClickListener(this);
+	    btn_led2.setOnClickListener(this);
+	    
         this.btn_all = (ToggleButton)this.findViewById(R.id.toggleButtonLEDAll);
         btn_all.setOnClickListener(this);
     }
@@ -34,7 +36,7 @@ public class MainActivity extends AbstractIOIOActivity implements View.OnClickLi
 	public void onClick(View v) {
 		switch(v.getId()){
 		case R.id.toggleButtonLED0:
-			if(this.btn_led[0].isChecked()){
+			if(this.btn_led0.isChecked()){
 				log("LED0 ON");
 			}
 			else{
@@ -42,7 +44,7 @@ public class MainActivity extends AbstractIOIOActivity implements View.OnClickLi
 			}
 			break;
 		case R.id.toggleButtonLED1:
-			if(this.btn_led[1].isChecked()){
+			if(this.btn_led1.isChecked()){
 				log("LED1 ON");
 			}
 			else{
@@ -50,7 +52,7 @@ public class MainActivity extends AbstractIOIOActivity implements View.OnClickLi
 			}
 			break;
 		case R.id.toggleButtonLED2:
-			if(this.btn_led[2].isChecked()){
+			if(this.btn_led2.isChecked()){
 				log("LED2 ON");
 			}
 			else{
@@ -68,12 +70,27 @@ public class MainActivity extends AbstractIOIOActivity implements View.OnClickLi
 	
 	class IOIOThread extends AbstractIOIOActivity.IOIOThread{
 		
-		protected void loop() throws ioio.lib.api.exception.ConnectionLostException{
+		private DigitalOutput[] led = new DigitalOutput[3];
+		
+		protected void setup() throws ConnectionLostException{
+			led[0] = this.ioio_.openDigitalOutput(0);
+			led[1] = this.ioio_.openDigitalOutput(1);
+			led[2] = this.ioio_.openDigitalOutput(2);
+		}
+		
+		protected void loop() throws ConnectionLostException{
+			led[0].write(btn_led0.isChecked());
+			led[1].write(btn_led1.isChecked());
+			led[2].write(btn_led2.isChecked());
 			try {
                 sleep(10);
         	}
 			catch (InterruptedException e) {
         	}
+		}
+		
+		public void led(int pin, boolean on_off) throws ConnectionLostException{
+			led[pin].write(on_off);
 		}
 	}
 	
